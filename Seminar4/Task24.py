c85cab2e94260af566ad2319915a9c8079f53392
Igor <shi86@bk.ru>
2023-07-27 10:17:31 +0500

    Task24.py is changed

--- Seminar4/Task24.py
+++ Seminar4/Task24.py
@@ -1,16 +1,17 @@
 from random import randint
 import os
 clear = lambda: os.system('cls')
-kust = list(randint(1, 10) for i in range(int(input('Введите кол-во кустов: '))))
-print(kust)
-a = int(input('Введите № куста: '))
-res = 0
-if a == 1:
-    res = kust[0] + kust[1] + kust[-1]
-elif a == len(kust):
-    res = kust[-2] + kust[-1] + kust[0]
-else:
-    res = kust[a-1] + kust[a-2] + kust[a]
+res = int(input('Введите кол-во кустов: '))
+kust = list(randint(1, 10) for i in range(res))
+max=kust[-1]+kust[0]+kust[1]
+
+if max<kust[len(kust)-1]+kust[len(kust)-2]+kust[0]:
+    max=kust[len(kust)-1]+kust[len(kust)-2]+kust[0]
+
+for i in range(1,len(kust)-1):
+    if max<kust[i-1]+kust[i]+kust[i+1]:
+        max=kust[i-1]+kust[i]+kust[i+1]
+        
 clear()
-print(f'{a}->{kust}')
-print(res)
+print(f'{res}->{kust}')
+print(max)
